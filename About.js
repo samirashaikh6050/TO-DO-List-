b73ab66453455af3271426a026595cadf7416350
samirashaikh6050 <85743999+samirashaikh6050@users.noreply.github.com>
2022-06-22 12:41:26 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Made a Todo List. Where we can add the work we needs to complete and then delete it later when done....  </p>
+    </div>
+  )
+}
+
+export default About
---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">
+      Copyright &copy; MytodosList.com
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
